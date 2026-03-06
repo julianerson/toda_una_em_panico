007b324b8d14fd6c1702bc9d20e18857742ccf43
trybulet <nilson.m.b.abreu@gmail.com>
2026-03-06 06:01:21 -0300

    versao 1.2, agora temos separaçoes para formulas

--- toda_una_em_panico.py
+++ toda_una_em_panico.py
@@ -15,23 +15,24 @@ def informacoes():#informaçoes
       para potencia ? (ex: 3?2 = 9)
 estamos em atualizaçoes desculpe''')
 
-def integraçoes_definidas():#integraçao definida
-      print('seja bem vindo, vamos fazer integraçoes simples definidas')
-      denifiçoes1 = int(input('diga o inicio de onde vamos integrar'))#a parte do inicio da area que vamos calcular
-      denifiçoes2 = int(input('otimo, diga o fim de onde vamos integrar'))#a parte final
-      varia = sympy.symbols(input('diga as variaveis e as separe com espaço'))#aqui ode encaixa as variaveis
-      formulastr = input('printe sua formula').replace('?', '**')#aqui e colocado a formula
-      formulas = parse_expr(formulastr, transformations=transformations)#aqui a formula e traduzida para o sympy
-      integral = sympy.integrate(formulas,(varia, denifiçoes1, denifiçoes2))#temos a integral em si
-      print(f'a sua integral de {denifiçoes1} ate {denifiçoes2} vai gerar {integral}')#seu resultado
+class calculo:#noçoes de calculo
+     def integraçoes_definidas(self):#integraçao definida
+          print('seja bem vindo, vamos fazer integraçoes simples definidas')
+          denifiçoes1 = int(input('diga o inicio de onde vamos integrar'))#a parte do inicio da area que vamos calcular
+          denifiçoes2 = int(input('otimo, diga o fim de onde vamos integrar'))#a parte final
+          varia = sympy.symbols(input('diga as variaveis e as separe com espaço'))#aqui ode encaixa as variaveis
+          formulastr = input('printe sua formula').replace('?', '**')#aqui e colocado a formula
+          formulas = parse_expr(formulastr, transformations=transformations)#aqui a formula e traduzida para o sympy
+          integral = sympy.integrate(formulas,(varia, denifiçoes1, denifiçoes2))#temos a integral em si
+          print(f'a sua integral de {denifiçoes1} ate {denifiçoes2} vai gerar {integral}')#seu resultado
 
-def integraçoes_indefinidas():#quase a mesma bosta da integral definida
-    print('ola bem vindo as integrais indefinidas, por enquanto de variavels temos so x vamos atualizar')#preciso explicar?
-    varia = sympy.symbols(input('diga as variaveis e as separe com espaço'))#variaveis
-    formulastr = input('printe sua formula').replace('?', '**')#formula
-    formulas = parse_expr(formulastr, transformations=transformations)#traduçao da formula para o sympy
-    integral01 = sympy.integrate(formulas, varia)#aqui fazemos as integrais
-    print(f'a sua formula tem a seguinte integral {integral01} + C')#vc realmente acha que iria esquecer do C? vc esta quase certo
+     def integraçoes_indefinidas(self):#quase a mesma bosta da integral definida
+          print('ola bem vindo as integrais indefinidas, por enquanto de variavels temos so x vamos atualizar')#preciso explicar?
+          varia = sympy.symbols(input('diga as variaveis e as separe com espaço'))#variaveis
+          formulastr = input('printe sua formula').replace('?', '**')#formula
+          formulas = parse_expr(formulastr, transformations=transformations)#traduçao da formula para o sympy
+          integral01 = sympy.integrate(formulas, varia)#aqui fazemos as integrais
+          print(f'a sua formula tem a seguinte integral {integral01} + C')#vc realmente acha que iria esquecer do C? vc esta quase certo
 
 while True:#aqui onde o user começa a ver
      AGUI = inquirer.select(
@@ -44,9 +45,9 @@ while True:#aqui onde o user começa a ver
           informacoes()
           continue
      elif 'integracao indefinida' in AGUI:
-          integraçoes_indefinidas()
+          calculo.integraçoes_indefinidas()
           print('otimo, temos mais contas otaro')
           continue
      elif 'integraçao definida' in AGUI:
-          integraçoes_definidas()
+          calculo.integraçoes_definidas()
           continue
