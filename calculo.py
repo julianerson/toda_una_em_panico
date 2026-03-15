b59e9350dc6fd622b646b4ed9c75175034a7adc0
trybulet <nilson.m.b.abreu@gmail.com>
2026-03-15 20:43:45 -0300

    o C ja faz o mesmo somatorio

--- calculo.py
+++ calculo.py
@@ -23,13 +23,4 @@ class calculo:#noçoes de calculo
           formulas = parse_expr(formulastr, transformations=transformations)#traduçao da formula para o sympy
           integral01 = sympy.integrate(formulas, varia)#aqui fazemos as integrais
           print(f'a sua formula tem a seguinte integral {integral01} + C')#vc realmente acha que iria esquecer do C? vc esta quase certo
-     def somatorio_simples(self):
-         repeti = int(input('quantas vezes vai ser repetido?'))
-         i = int(input('qual e o i'))
-         formula = input('diga a formula').replace('?','**')
-         formulasim = 0
-         for o in range(repeti):
-             formula2 = eval(formula)
-             formulasim = formulasim + formula2
-             i = i + 1
-         print(f'a resposta e {formulasim}')
+
