30ef6469935006aa7c4c2225b5b74d885f003025
trybulet <nilson.m.b.abreu@gmail.com>
2026-03-19 17:42:30 -0300

    instalador comentarios

--- instalador.py
+++ instalador.py
@@ -3,8 +3,8 @@ import subprocess
 from time import sleep as mimir
 #isso deve compilar o .c
 nome = os.name
-subprocess.run("pip install scipy sympy InquirerPy")
-if nome == "nt":
-    subprocess.run(r"gcc -shared -o calculadora.dll calculadora.c tinyexpr.c -Wl,--add-stdcall-alias",shell=True)
-elif nome == "posix":
-    subprocess.run(r"gcc -shared -fPIC calculadora.c tinyexpr.c -o calculadora.so -lm",shell=True)
+subprocess.run("pip install scipy sympy InquirerPy")#instala as bibliotecas do python
+if nome == "nt":#ve se e windows
+    subprocess.run(r"gcc -shared -o calculadora.dll calculadora.c tinyexpr.c -Wl,--add-stdcall-alias",shell=True)#compila para windows
+elif nome == "posix":#ve se e linux
+    subprocess.run(r"gcc -shared -fPIC calculadora.c tinyexpr.c -o calculadora.so -lm",shell=True)#compila para linux
