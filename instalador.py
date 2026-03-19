5e18ca74b62e5e4cdaec2eb6c78d057bac1164b6
trybulet <nilson.m.b.abreu@gmail.com>
2026-03-19 17:39:47 -0300

    instalador agora baixa as bibliotecas

--- instalador.py
+++ instalador.py
@@ -3,7 +3,7 @@ import subprocess
 from time import sleep as mimir
 #isso deve compilar o .c
 nome = os.name
-
+subprocess.run("pip install scipy sympy InquirerPy")
 if nome == "nt":
     subprocess.run(r"gcc -shared -o calculadora.dll calculadora.c tinyexpr.c -Wl,--add-stdcall-alias",shell=True)
 elif nome == "posix":
