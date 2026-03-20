b12510aa64b2b7ea7fceb445304172bf9ca38a05
trybulet <nilson.m.b.abreu@gmail.com>
2026-03-20 11:24:22 -0300

    consertei o bug de instalação de bibliotecas

--- instalador.py
+++ instalador.py
@@ -3,7 +3,7 @@ import subprocess
 from time import sleep as mimir
 #isso deve compilar o .c
 nome = os.name
-subprocess.run("pip install scipy sympy InquirerPy")#instala as bibliotecas do python
+subprocess.run("pip install scipy sympy InquirerPy",shell=True)#instala as bibliotecas do python
 if nome == "nt":#ve se e windows
     subprocess.run(r"gcc -shared -o calculadora.dll calculadora.c tinyexpr.c -Wl,--add-stdcall-alias",shell=True)#compila para windows
 elif nome == "posix":#ve se e linux
