f70344e1c3e14efa7643b3cb2aeb4bdb1731e70b
trybulet <nilson.m.b.abreu@gmail.com>
2026-05-24 13:21:09 -0300

    rdtfgxd

--- calculadora.c
+++ calculadora.c
@@ -265,7 +265,7 @@ void somatorio(){//funçao somatorio
     char comando[20];
     double i = 0;//diz o i
 
-    printf("você quer somatorio,produtorio\n");
+    printf("você quer somatorio ou produtorio ?\n");
     scanf(" %s",&comando);
     limpar_tela();
 
